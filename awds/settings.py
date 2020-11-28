7caf178d5e54852e30c05c61e27c9f087c4756de
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-28 11:07:45 +0300

    python decouple and not django-decouple

--- awds/settings.py
+++ awds/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'tinymce',
     'bootstrap4',
     'cloudinary',
     'awwards',
@@ -165,6 +166,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
