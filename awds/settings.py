0b1ce557c639e4e9991bbd36a08262ef85f1b495
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-28 09:49:09 +0300

    procfile

--- awds/settings.py
+++ awds/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%%zr&1^)0in#n@l(viykz_nm)5#mit=)$mj_t-aeu)9jl1qc)o'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'awwards',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
