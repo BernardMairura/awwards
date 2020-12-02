bdb0f5616bd01621d0733ccc3cc090b3f622dcbb
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-12-02 08:45:48 +0300

    regular expressions

--- awwards/urls.py
+++ awwards/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    re_path('^$',views.home,name='home'),
+    re_path(r'^$',views.home,name='home'),
     re_path(r'^projects/(\d+)',views.projects,name='projects'),
     path('profile/<int:user_id>', views.profile, name='profile'),
     re_path(r'^uploads/', views.uploadproject, name='uploadproject'),
